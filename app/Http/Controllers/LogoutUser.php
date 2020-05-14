522da7a3e770f956dab852fd4305faca5378c42f
Karl Hughes <khughes.me@gmail.com>
2020-05-14 18:24:17 -0500

    Updating logout with fusionauth

--- app/Http/Controllers/LogoutUser.php
+++ app/Http/Controllers/LogoutUser.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use FusionAuth\FusionAuthClient;
+
 class LogoutUser extends Controller
 {
+    private $authClient;
+
+    public function __construct(FusionAuthClient $authClient)
+    {
+        $this->authClient = $authClient;
+    }
+
     public function __invoke()
     {
+        $this->authClient->logout(false);
         session()->flush();
         session()->flash('You have been logged out.');
 
